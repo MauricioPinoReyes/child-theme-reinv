5ff66077523f16273f89e563b056454a967c0088
unknown <mauricio.pino.log@gmail.com>
2022-01-28 10:18:40 -0300

    primer commit header y slider agreagados

---
+++ header.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- styles CSS -->
+  <link rel="stylesheet" href="wp-content\themes\twentytwentyone-child\style.css">
+
+
+
+  <title>Cabecera</title>
+</head>
+
+<body>
+  <!--HEADER-->
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" width="250" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+
+        <ul class="navbar-nav ">
+          <li class="nav-item ">
+            <a class="nav-link" href="#">Conócenos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nuestro impacto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Programas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary " href="#">Donar</a>
+          </li>
+        </ul>
+
+
+      </div>
+    </nav>
+  </div>
+
+  <!--HEADER-->
+
+  <!--CAROUSEL BOOTSTRAP-->
+
+  <div class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/img_1.jpeg" width="700px" height="400px" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <p>Capacitación, apoyo y desarrollo para la reinserción</p>
+            <h5>Contruyamos una sociedad </h5>
+            <h5>de oportunidades </h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/img_2.jpeg" height="400px" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <p>Capacitación, apoyo y desarrollo para la reinserción</p>
+            <h5>Contruyamos una sociedad </h5>
+            <h5>de oportunidades </h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/img_3.jpeg" height="400px" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <p>Capacitación, apoyo y desarrollo para la reinserción</p>
+            <h5>Contruyamos una sociedad </h5>
+            <h5>de oportunidades </h5>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+
+  <!--CAROUSEL BOOTSTRAP-->
